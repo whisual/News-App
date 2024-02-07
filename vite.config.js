5d3705e4be988557f7540d34cc1d4eabb8a7c18b
Vishal Sharma <102051680+whisual@users.noreply.github.com>
2024-02-07 21:22:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  proxy: {
+    '/api': {
+      target: 'https://news-app-sandy-gamma.vercel.app/',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, ''),
+    },
   plugins: [react()],
 })
